d8c77b50681a99739bbf90ccf9da37b3ff863c7c
Enzamul Islam <77445154+aamibhoot@users.noreply.github.com>
2024-11-15 21:37:07 +0600

    Update app.config.ts
    Added SMART BD info

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,13 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  title: 'SMART',
+  email: 'fyi.apon@smartbd.com',
+  github: 'https://github.com/aamibhoot',
+  twitter: 'https://sink.cool/',
+  telegram: 'https://sink.cool/',
+  mastodon: 'https://sink.cool/',
+  blog: 'https://sink.cool/',
+  description: 'Welcome to SMART BD!',
+  image: 'https://smartbd.com/wp-content/uploads/2024/07/Smart-Web-Banner-Design-microsoft-old.jpg',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
