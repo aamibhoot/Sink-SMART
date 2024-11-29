4fed85686b00d1ff43db919807da24354818174f
Aami Bhoot <bhootaami@gmail.com>
2024-11-29 17:33:40 +0600

    Add route handling to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
+<script setup>
+import { useRoute } from 'vue-router'; // Import the route composable
+
+const route = useRoute(); // Get the current route
+</script>
+
 <template>
-  <main class="flex flex-col min-h-screen">
+  <main v-if="route.path !== '/'" class="flex flex-col min-h-screen">
     <LayoutsHeader />
     <section class="flex flex-1">
       <div class="container mx-auto">
